b5e41c704718a45cfda259c690f2adfa37ee87f1
Smoghadasi <sadra.19@gmail.com>
2021-03-25 10:53:39 +0430

    Change web.php

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,5 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
 })->name('dashboard');
 
 Route::get('/{any}', 'App\Http\Controllers\PagesController@index')->where('any', '.*');
+
+//یک تغییر ایجاد شد
